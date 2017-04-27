5cf16737906fe4030c9e3ae401c1cf23afa218fa
Kikju <kiikju@gmail.com>
2017-04-27 19:56:47 +0200

    added flowable and maybe

---
+++ rxparallel/src/main/java/de/wirecard/rxparallel/FlowableParallel.java
@@ -0,0 +1,41 @@
+package de.wirecard.rxparallel;
+
+import org.reactivestreams.Subscriber;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import io.reactivex.subjects.Subject;
+
+public class FlowableParallel<FLOWABLE, PARALLEL> extends Flowable<FLOWABLE> {
+
+    private Flowable<FLOWABLE> mainFlowable;
+    private Subject<PARALLEL> parallelSubject;
+
+    public FlowableParallel(Flowable<FLOWABLE> mainFlowable, Subject<PARALLEL> parallelSubject) {
+        this.mainFlowable = mainFlowable;
+        this.parallelSubject = parallelSubject;
+    }
+
+    @Override
+    protected void subscribeActual(Subscriber<? super FLOWABLE> observer) {
+        mainFlowable.subscribe(observer);
+    }
+
+    public Flowable<FLOWABLE> subscribeParallel(Observer<PARALLEL> parallelObserver) {
+        if (this.parallelSubject != null && parallelObserver != null) {
+            this.parallelSubject.subscribeWith(parallelObserver);
+        }
+        return mainFlowable;
+    }
+
+    public static <FLOWABLE, PARALLEL> Function<? super Flowable<FLOWABLE>, FlowableParallel<FLOWABLE, PARALLEL>> with(final Subject<PARALLEL> parallelSubject) {
+        return new Function<Flowable<FLOWABLE>, FlowableParallel<FLOWABLE, PARALLEL>>() {
+            @Override
+            public FlowableParallel<FLOWABLE, PARALLEL> apply(@NonNull Flowable<FLOWABLE> flowable) throws Exception {
+                return new FlowableParallel<FLOWABLE, PARALLEL>(flowable, parallelSubject);
+            }
+        };
+    }
+}
---
+++ rxparallel/src/main/java/de/wirecard/rxparallel/MaybeParallel.java
@@ -0,0 +1,40 @@
+package de.wirecard.rxparallel;
+
+import io.reactivex.Maybe;
+import io.reactivex.MaybeObserver;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import io.reactivex.subjects.Subject;
+
+public class MaybeParallel<MAYBE, PARALLEL> extends Maybe<MAYBE> {
+
+    private Maybe<MAYBE> mainMaybe;
+    private Subject<PARALLEL> parallelSubject;
+
+    public MaybeParallel(Maybe<MAYBE> mainMaybe, Subject<PARALLEL> parallelSubject) {
+        this.mainMaybe = mainMaybe;
+        this.parallelSubject = parallelSubject;
+    }
+
+    @Override
+    protected void subscribeActual(MaybeObserver<? super MAYBE> observer) {
+        mainMaybe.subscribe(observer);
+    }
+
+    public Maybe<MAYBE> subscribeParallel(Observer<PARALLEL> parallelObserver) {
+        if (this.parallelSubject != null && parallelObserver != null) {
+            this.parallelSubject.subscribeWith(parallelObserver);
+        }
+        return mainMaybe;
+    }
+
+    public static <MAYBE, PARALLEL> Function<? super Maybe<MAYBE>, MaybeParallel<MAYBE, PARALLEL>> with(final Subject<PARALLEL> parallelSubject) {
+        return new Function<Maybe<MAYBE>, MaybeParallel<MAYBE, PARALLEL>>() {
+            @Override
+            public MaybeParallel<MAYBE, PARALLEL> apply(@NonNull Maybe<MAYBE> maybe) throws Exception {
+                return new MaybeParallel<MAYBE, PARALLEL>(maybe, parallelSubject);
+            }
+        };
+    }
+}
